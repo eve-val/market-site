2813323691cdd38910d007c8ff243384c87b05e7
Michael Sullivan <sully@msully.net>
2013-09-03 16:15:58 -0400

    Update the update.sh script.

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git pull && ./market-stuff.py > index.html && ./market-stuff.py --poller > id_list.js && scp id_list.js index.html poller.html nfsn:/home/public/stuff/market/
+git pull && ./market-stuff.py > index.html && ./market-stuff.py --poller > id_list.js && scp id_list.js index.html poller.html sound_market:
