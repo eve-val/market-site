49816c51baaa8885e14f9b4238b144269790b066
Michael Sullivan <sully@msully.net>
2013-12-03 16:12:42 -0500

    Get data for lsc4.

--- update_data.sh
+++ update_data.sh
@@ -2,4 +2,5 @@
 ./market-stuff.py VLGD-R > vlgd-r.html && \
 ./market-stuff.py Orvolle > orvolle.html && \
 ./market-stuff.py 9GYL-O > 9guy.html && \
+./market-stuff.py LSC4-P > lsc4-p.html && \
 ./market-stuff.py --poller > id_list.js
