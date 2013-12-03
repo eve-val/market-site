699e7322adb73daf468fa5f8f80dbb51c05582e0
Michael Sullivan <sully@msully.net>
2013-12-03 16:47:13 -0500

    Make market-stuff output the html files directly.

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git pull && \
-./update_data.sh && \
-scp id_list.js market.css vlgd-r.html orvolle.html 9guy.html lsc4-p.html poller.html sound_market:
+./market-stuff.py VLGD-R Orvolle 9GYL-O LSC4-P && \
+scp id_list.js market.css *.html sound_market:
