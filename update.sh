d793bc844c6c8bd1a9ef156cbb5143ad7010deba
Michael Sullivan <sully@msully.net>
2014-09-10 14:42:21 -0400

    Only update EX6

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@ echo -n "Update started at "
 date
 git pull && \
 echo "Running market updater" &&
-timeout 10m ./market-stuff.py EX6-AO LSC4-P && \
+timeout 10m ./market-stuff.py EX6-AO && \
 echo "Market updater finished" &&
 scp id_list.js market.css *.html sound_market:
