cad7616e4a7424115b8d234d9855f519c581a75b
Michael Sullivan <sully@msully.net>
2014-09-10 14:40:45 -0400

    Wrap the market script in a timeout

--- update.sh
+++ update.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+echo -n "Update started at "
+date
 git pull && \
-./market-stuff.py EX6-AO LSC4-P && \
+echo "Running market updater" &&
+timeout 10m ./market-stuff.py EX6-AO LSC4-P && \
+echo "Market updater finished" &&
 scp id_list.js market.css *.html sound_market:
