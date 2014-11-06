e4895e0753cf035d8fad22aa22b6299744151d76
Michael Sullivan <sully@msully.net>
2014-11-06 00:51:34 -0500

    Update 4-07 also.

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 echo -n "Update started at "
 date
 echo "Running market updater" &&
-timeout 10m ./market-stuff.py EX6-AO && \
+timeout 10m ./market-stuff.py 4-07MU EX6-AO && \
 echo "Market updater finished" &&
 scp id_list.js market.css *.html sound_market:
