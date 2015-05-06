b957301dab7a67ed0b0f2e487377542a3a91bd27
Michael Sullivan <sully@msully.net>
2015-05-06 15:10:28 -0400

    And 14Y too, I guess

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 echo -n "Update started at "
 date
 echo "Running market updater" &&
-timeout 10m ./market-stuff.py I-CUVX && \
+timeout 10m ./market-stuff.py I-CUVX 14YI-D && \
 echo "Market updater finished" &&
 scp id_list.js market.css *.html sound_market:
