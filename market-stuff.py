fc132a8c37406ad08f62168f7f2dda680c3aeb36
Michael Sullivan <sully@msully.net>
2017-04-05 19:34:24 -0400

    make the script loads https

--- market-stuff.py
+++ market-stuff.py
@@ -230,14 +230,14 @@ def html_output(table, system):
     page_template = """
 <html><head><title>%(system)s market data</title>
 <!-- DataTables CSS -->
-<link rel="stylesheet" type="text/css" href="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/css/jquery.dataTables.css">
+<link rel="stylesheet" type="text/css" href="https://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/css/jquery.dataTables.css">
 <link rel="stylesheet" type="text/css" href="market.css">
 
 <!-- jQuery -->
-<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>
 
 <!-- DataTables -->
-<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js"></script>
 <script type="text/javascript" charset="utf-8">
 
 // formatted numbers sorting based on http://datatables.net/plug-ins/sorting#formatted_numbers
