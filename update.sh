8b0b5357d795bbd0e0b4337f0e5cb45885e1738f
Michael Sullivan <sully@msully.net>
2017-04-05 23:00:14 -0400

    back to sound_market

--- update.sh
+++ update.sh
@@ -4,4 +4,4 @@ date
 echo "Running market updater" &&
 timeout 10m ./market-stuff.py 1021149293700@J134407 && \
 echo "Market updater finished" &&
-cp market.css *.html live-site/
+scp market.css *.html sound_market:
