9576a1ee7b82d5dfef5c45839511e70f88f95f6c
Michael Sullivan <sully@msully.net>
2017-04-25 15:05:26 -0400

    update update.sh

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 echo -n "Update started at "
 date
 echo "Running market updater" &&
-timeout 10m ./market-stuff.py 1021149293700@J134407 && \
+timeout 10m ./market-stuff.py "Dern's House of Pancakes@J134407" && \
 echo "Market updater finished" &&
 scp market.css *.html sound_market:
