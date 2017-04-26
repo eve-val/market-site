6009024c9c8809ea69675b358661e1a9a43f002e
Michael Sullivan <sully@msully.net>
2017-04-26 12:54:58 -0400

    move auto-updating item list into update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+
+# argument should be a URL to fetch item list from
+URL="$1"
+
 echo -n "Update started at "
 date
+
+# Fech new items
+mkdir -p backup_items/
+cp items backup_items/items_`date +%Y%m%d-%H%M`
+curl -s $URL | ./market-stuff.py --filter > items
+
+# Run the stuff
 echo "Running market updater" &&
 timeout 10m ./market-stuff.py "Dern's House of Pancakes@J134407" && \
 echo "Market updater finished" &&
