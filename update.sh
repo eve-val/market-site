5d33572f9eced1972542960fc2b401846c2bc4dc
Liz Fong-Jones <elizabeth@ctyalcove.org>
2020-11-05 02:46:05 -0500

    We live elsewhere now.

--- update.sh
+++ update.sh
@@ -19,6 +19,6 @@ fi
 
 # Run the stuff
 echo "Running market updater" &&
-timeout 10m ./market-stuff.py "Statio Tranquillitatis@J100820" && \
+timeout 10m ./market-stuff.py "International Space Station@LSC4-P" && \
 echo "Market updater finished" &&
 scp market.css *.html sound_market:
